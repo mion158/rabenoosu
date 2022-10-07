9c769531b2b5ae088eb16dc1a26dbcac20974293
mion158 <111487792+mion158@users.noreply.github.com>
2022-10-07 21:38:58 +0100

    test with one instance

---
+++ src/components/Business/Business.js
@@ -0,0 +1,37 @@
+import React from 'react';
+const business = {
+    imageSrc: 'https://64.media.tumblr.com/b3770fb1e35404abcdf79d31e23656e4/3fb43f56835d1cfd-0a/s500x750/14f58ce6435cf07f65037548929223b5c88a1a39.png',
+    name: 'Hatsune Miku',
+    address: '1010 Paddington Way',
+    city: 'Flavortown',
+    state: 'NY',
+    zipCode: '10101',
+    category: 'Italian',
+    rating: 4.5,
+    reviewCount: 90
+}
+class Business extends React.Component {
+    render() {
+        return (
+            <div className="Business">
+                <div className="image-container">
+                    <img src='https://64.media.tumblr.com/b3770fb1e35404abcdf79d31e23656e4/3fb43f56835d1cfd-0a/s500x750/14f58ce6435cf07f65037548929223b5c88a1a39.png' alt='' />
+                </div>
+                <h2>{business.name}</h2>
+                <div className="Business-information">
+                    <div className="Business-address">
+                        <p>{business.address}</p>
+                        <p>{business.city}</p>
+                        <p>{business.state} {business.zipCode}</p>
+                    </div>
+                    <div className="Business-reviews">
+                        <h3>{business.category}</h3>
+                        <h3 class="rating">{business.rating} stars</h3>
+                        <p>{business.reviewCount} reviews</p>
+                    </div>
+                </div>
+            </div>
+        )
+
+    }
+}

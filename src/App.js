877d0711bc492ac48d7352bd283cccedccc8b053
mion158 <111487792+mion158@users.noreply.github.com>
2022-10-18 20:27:19 +0100

    moved object

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ import './App.css';
 import BusinessList from './components/BusinessList/BusinessList';
 import SearchBar from './components/SearchBar/SearchBar';
 
+const business = {
+  imageSrc: 'https://64.media.tumblr.com/b3770fb1e35404abcdf79d31e23656e4/3fb43f56835d1cfd-0a/s500x750/14f58ce6435cf07f65037548929223b5c88a1a39.png',
+  name: 'Animu Pizza',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
+
 class App extends React.Component {
   render() {
     return (
--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -2,17 +2,6 @@ import React from 'react';
 import './Business.css';
 
 
-const business = {
-    imageSrc: 'https://64.media.tumblr.com/b3770fb1e35404abcdf79d31e23656e4/3fb43f56835d1cfd-0a/s500x750/14f58ce6435cf07f65037548929223b5c88a1a39.png',
-    name: 'Animu Pizza',
-    address: '1010 Paddington Way',
-    city: 'Flavortown',
-    state: 'NY',
-    zipCode: '10101',
-    category: 'Italian',
-    rating: 4.5,
-    reviewCount: 90
-}
 class Business extends React.Component {
     render() {
         return (

d72295580966ab597a4a777d7d2b3f74696c996a
mion158 <111487792+mion158@users.noreply.github.com>
2022-10-24 19:56:36 +0100

    updated

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,9 +1,18 @@
-apiKey = 'sorryitishidden'
+apiKey = ''
 
 //an object
 const Yelp = {
     search(term, location, sortBy){
-        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`);
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+        {headers: {
+            Authorization: `Bearer ${apiKey}`
+        }}).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if (jsonResponse.businesses) { //see if jsonresponse has a businesses key
+                return jsonResponse.businesses.map();
+            }
+        });
     }
 }
 

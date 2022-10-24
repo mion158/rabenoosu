7528b86617afcf3a3b4e55e6ce8cb7ab047e63db
mion158 <111487792+mion158@users.noreply.github.com>
2022-10-24 19:50:20 +0100

    tempt demo server

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -3,7 +3,7 @@ apiKey = 'sorryitishidden'
 //an object
 const Yelp = {
     search(term, location, sortBy){
-        return fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`);
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`);
     }
 }
 

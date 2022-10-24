d45684d53a22b517e69d37158f06407d06a60ea9
mion158 <111487792+mion158@users.noreply.github.com>
2022-10-24 16:41:25 +0100

    added sort class

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -10,6 +10,31 @@ const sortByOptions = {
 };
 
 class SearchBar extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            term: '',
+            location: '',
+            sortBy: 'best_match',
+
+        };
+        
+        this.sortByOption = {
+            'Best Match': 'best_match',
+            'Highest Rated': 'rating',
+            'Most Reviewed': 'review_count',
+        };
+    }
+
+
+
+    getSortedByClass(sortByOption){
+        if (this.state.sortBy === sortByOption) {
+            return 'active';
+        } 
+        return '';
+    }
+
     renderSortByOptions() {
         return Object.keys(sortByOptions).map(sortByOption => {
             let sortByOptionValue = sortByOptions[sortByOption];

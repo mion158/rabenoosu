a4a9dbbd19566acf7b52de3e2e4d7e7612e50ce6
mion158 <111487792+mion158@users.noreply.github.com>
2022-10-24 19:48:06 +0100

    url

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -3,7 +3,7 @@ apiKey = 'sorryitishidden'
 //an object
 const Yelp = {
     search(term, location, sortBy){
-        return fetch();
+        return fetch(`https://api.yelp.com/v3/businesses/search?term=TERM&location=LOCATION&sort_by=SORT_BY`);
     }
 }
 
